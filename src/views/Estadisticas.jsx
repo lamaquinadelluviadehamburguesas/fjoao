433d4c7e8af2f2c2750e0bab4c870aee99552545
joao <j1a5j4v0er@gmail.com>
2025-05-21 16:42:49 -0600

    grafico

--- src/views/Estadisticas.jsx
+++ src/views/Estadisticas.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import VentasPorMes from '../components/graficos/VentasPorMes';
+import VentasPorEmpleado from '../components/graficos/VentasPorEmpleado';
 
 const Estadisticas = () => {
   // Declare state variables for chart data
   const [meses, setMeses] = useState([]);
   const [totalesPorMes, setTotalesPorMes] = useState([]);
+  const [empleados, setEmpleados] = useState([]);
+  const [total_ventas, setTotalVentas] = useState([]);
 
   // Fetch data on component mount
   useEffect(() => {
@@ -20,16 +23,32 @@ const Estadisticas = () => {
         alert('Error al cargar ventas: ' + error.message);
       }
     };
+    const cargaVentasPorEmpleado = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/totalVentasPorEmpleado');
+        const data = await response.json();
+        setEmpleados(data.map(item => item.primer_nombre + " " + item.primer_apellido));
+        setTotalVentas(data.map(item => item.total_ventas));
+      } catch (error) {
+        console.error('Error al cargar ventas por empleado:', error);
+        alert('Error al cargar ventas por empleado: ' + error.message);
+      }
+    };
+
     cargaVentas();
+    cargaVentasPorEmpleado();
   }, []);
 
-  // Render the VentasPorMes component with fetched data
+  // Render the VentasPorMes and VentasPorEmpleado components with fetched data
   return (
     <Container className="mt-5">
       <h4>Estadísticas</h4>
       <Row className="mt-4">
         <Col xs={12} sm={12} md={12} lg={6} className="mb-4">
           <VentasPorMes meses={meses} totales_por_mes={totalesPorMes} />
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={6} className="mb-4">
+          <VentasPorEmpleado empleados={empleados} total_ventas={total_ventas} />
         </Col>
       </Row>
     </Container>
